06f9ad9e2fd9f7ecf2e5b74080d7b090a18ac076
Rommel Canoy <rommelcanoy4@gmail.com>
2022-01-29 22:37:57 +0800

    add: add a function that prevent the form to refresh, and store the input values in an object

--- src/Create.js
+++ src/Create.js
@@ -5,24 +5,29 @@ const Create = () => {
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author }
+
+    console.log(blog);
+  }
+
   return ( 
     <div className="create">
       <h2>Add a new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)}/>
         <label>Blog body:</label>
         <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
         <label>Blog author:</label>
-        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+        <select required value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="">Please select an author</option>
           <option value="rommel">rommel</option>
           <option value="brian">brian</option>
         </select>
         <button>Add blog</button>
       </form>
-      <p>{title}</p>
-      <p>{body}</p>
-      <p>{author}</p>
     </div>
    );
 }
