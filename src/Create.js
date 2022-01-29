4db3f03f17c6273d03a5e62cdfac3d1d764b42f4
Rommel Canoy <rommelcanoy4@gmail.com>
2022-01-29 22:46:55 +0800

    update: update form to save form data towards db.json through post request

--- src/Create.js
+++ src/Create.js
@@ -4,12 +4,25 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('');
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author }
 
-    console.log(blog);
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('New blog added');
+      setIsPending(false);
+      setTitle('')
+      setBody('')
+      setAuthor('')
+    })
   }
 
   return ( 
@@ -26,7 +39,8 @@ const Create = () => {
           <option value="rommel">rommel</option>
           <option value="brian">brian</option>
         </select>
-        <button>Add blog</button>
+        {!isPending && <button>Add blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
       </form>
     </div>
    );
